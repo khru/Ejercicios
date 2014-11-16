f44d6faccb386c6c7c25f257d490ebdc96dafd34
khru <evrtrabajo@gmail.com>
2014-11-16 18:53:38 +0100

    Create Ejercicio1.1.6 Tabla.java

---
+++ Ejercicio1.1.6 Tabla.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+/**
+ * @author Emmanuel Valverde Ramos
+ *
+ */
+public class Tabla
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		Scanner teclado = new Scanner(System.in);
+		int num;
+		System.out.println("Introduce que tabla deseas visualizar del 1 al 15: ");
+		num = teclado.nextInt();
+		if((num <= 0) || (num > 15) )
+		{
+			error();
+		}
+		else 
+		{
+			tabla(num);
+		}
+		
+	}
+	public static void tabla(int num)
+	{
+		int multiplicar = 0;
+		for(int i = 1;i <= num;i++)
+		{
+			System.out.println();
+			System.out.println("Tabla del "+i);
+			for (int j = 1;j < 10;j++)
+			{
+				multiplicar = i * j;
+				System.out.println(i+" * "+j+" = "+ multiplicar);
+			}
+		}
+	}
+	public static void error()
+	{
+		System.out.println("El valor introducido no es valido");
+	}
+
+}
