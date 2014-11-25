c613db580f772b05e480f5b6e5f9fb0320fe6468
khru <evrtrabajo@gmail.com>
2014-11-25 16:52:14 +0100

    Update EjercicioAmarillo2_4.1.2.java

--- EjercicioAmarillo2_4.1.2.java
+++ EjercicioAmarillo2_4.1.2.java
@@ -11,15 +11,15 @@ public class EjercicioAmarillo2
 
 	public static void main(String[] args) 
 	{
-			int vec[] = new int[20];
-			for(int i = 0;i < vec.length;i++)
-			{
-				vec[i] = i * 5;
-			}
-			for(int i = 0;i < vec.length;i++)
-			{
-				System.out.println("El valor del array en la posición "+i+" es: "+vec[i]);
-			}
+		int vec[] = new int[20];
+		for(int i = 0;i < vec.length;i++)
+		{
+			vec[i] = i * 5;
+		}
+		for(int i = 0;i < vec.length;i++)
+		{
+			System.out.println("El valor del array en la posición "+i+" es: "+vec[i]);
+		}
 	}
 
 }
