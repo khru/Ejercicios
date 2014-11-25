f78421cfd3739b67fd84d145410394b90fbb4de0
khru <evrtrabajo@gmail.com>
2014-11-25 21:16:43 +0100

    Create EjercicioVectoresDoblesDistintos.java

---
+++ EjercicioVectoresDoblesDistintos.java
@@ -0,0 +1,28 @@
+package Programación412;
+
+/**
+ * @author khru
+ *
+ */
+public class EjercicioLength {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int vec1[][] = {{1,2,3,4},
+					   {5,6},
+					   {7,8,9}};
+		
+		for(int i = 0;i < vec1.length;i++)
+		{
+			for(int j = 0;j < vec1[i].length;j++)
+			{
+				System.out.println(vec1[i][j]);
+			}
+		}
+	}
+
+}
