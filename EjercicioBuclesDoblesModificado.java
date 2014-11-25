759d91c1ff0afda0973e5c4c8dc7c6ad1967e1bd
khru <evrtrabajo@gmail.com>
2014-11-25 21:19:47 +0100

    Create EjercicioBuclesDoblesModificado.java

---
+++ EjercicioBuclesDoblesModificado.java
@@ -0,0 +1,27 @@
+
+/**
+ * @author khru
+ *
+ */
+public class EjercicioLength {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int vec1[][] = {{1,2,3,4},
+					   {5,6},
+					   {7,8,9}};
+		
+		for(int [] t: vec1)
+		{
+			for(int n: t)
+			{
+				System.out.println(n);
+			}
+		}
+	}
+
+}
