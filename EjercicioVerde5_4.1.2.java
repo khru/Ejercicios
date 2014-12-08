52277281bc261b1e8d6c86dddb9d45e01ad54de8
khru <evrtrabajo@gmail.com>
2014-12-08 22:05:15 +0100

    Update EjercicioVerde5_4.1.2.java

--- EjercicioVerde5_4.1.2.java
+++ EjercicioVerde5_4.1.2.java
@@ -1,37 +1,37 @@
 package Programación412;
-
 /**
  * @author khru
- *
  */
 public class EjercicioVerde5 
 {
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
 		int vec[] = {1,2,3,4,5,6,7,8,9,0};
 		int num = 5;
-		buscarInt(vec, num);
+		int intbuscado;
+		intbuscado = buscarInt(vec, num);
+		if(intbuscado == -1)
+		{
+			System.out.println("El número no se encuentra en el Array");
+		}
+		else
+		{
+			System.out.println("La primera instancia esta en la posición "+intbuscado);
+		}
 	}
 	/**
-	 * 
 	 * @param vec
 	 * @param num
 	 */
-	static void buscarInt(int vec[], int num)
+	static int buscarInt(int vec[], int num)
 	{
-		int cont = 0;
-		for(int i = 0;(i < vec.length) && (vec[i] != num);i++)
+		for(int i = 0;i < vec.length;i++)
 		{
-			cont++;
-			/*if(vec[i] == num)
+			if(vec[i] == num)
 			{
-				System.out.println(i);
-				break;
+				return i;
 			}
-			System.out.println(i);*/
 		}
-		System.out.println(cont);
+		return -1;
 	}
-
 }
