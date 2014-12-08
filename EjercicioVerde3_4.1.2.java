37d7d2079efe8c429c7ec0b873d04a4b5ffe8614
khru <evrtrabajo@gmail.com>
2014-12-08 20:14:27 +0100

    Update EjercicioVerde3_4.1.2.java

--- EjercicioVerde3_4.1.2.java
+++ EjercicioVerde3_4.1.2.java
@@ -1,5 +1,5 @@
+package Programación412;
 import java.util.*;
-
 /**
  * @author khru
  *
@@ -8,21 +8,48 @@ public class EjercicioVerde3
 {
 	public static void main(String[] args) 
 	{
-		
+		int compararvectorchar = 0;
 		char vec[] = {'*','c','d'};
 		char vec1[] = {'*', 'c','d'};
-		comparaVectorChar(vec,vec1);
-	}
-	static void comparaVectorChar(char vec[], char vec1[])
-	{
-		boolean iguales = Arrays.equals(vec, vec1);
-		if(iguales == false)
+		if(vec.length != vec1.length)
 		{
-			System.out.println("No son iguales");
+			System.out.println("La longitud de los vectores es distinta, no se puede calcular");
 		}
 		else
 		{
-			System.out.println("son iguales");
+			compararvectorchar = comparaVectorChar(vec,vec1);
+			System.out.println(compararvectorchar);
 		}
 	}
+	/**
+	 * 
+	 * @param vec
+	 * @param vec1
+	 */
+	static int comparaVectorChar(char vec[], char vec1[])
+	{	
+		boolean iguales = Arrays.equals(vec, vec1);
+			if(iguales == false)
+			{
+				System.out.println("Los son diferentes");
+				for(int i = 0;i < vec.length;i++)
+				{
+					if((int)vec[i] > (int)vec1[i])
+						{
+							return 1;
+						}
+					else if((int)vec[i] < (int)vec1[i])
+					{
+						return -1;
+					}
+				}
+				return 0;
+			}
+			else
+			{
+				System.out.println("Los vectores son iguales");
+				return 0;
+			}
+	}
+}
 }
